2b0bcccaf6190393c7ab79cffec19dc4d39680fe
jeremyimmanuel <jeremytandjung@icloud.com>
2021-02-15 16:43:17 -0800

    started on docviewer

---
+++ src/components/DocViewer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DocViewer',
+
+    props: {
+        url: String
+    },
+
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
